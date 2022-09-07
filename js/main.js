bdeeba00c0328bba491b89523f6efcab10d35d12
Irving Alexis Pascual Hernandez <hpascual117gmail.com>
2022-09-06 21:16:58 -0500

    chore | se modifico el archivo main.js para poder realizar de manera correcta el ejercicio 2

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
-// Escribir una función que tome un arreglo de números y devuelva el número mayor de la lista. Ejemplo del arreglo : [3, 5, 7, 1, 6]
-let numeros = ["3", "5", "7", "1", "6"]; //aqui se declara los numeros del arreglo
-console.log("El número mayor en el arreglo " + numeros + " es: ");// este es el msj que mandara la consola del resultado
-console.log(numMayor(numeros));//aqui se inserta la variable a la funcion 
+// María llega a su entrevista de trabajo para el rol de Desarrollador Junior y se le pide que resuelva este problema: Dada una lista de números 
+//[-1, 3, 4, 2, 6], escribe una función en javascript para calcular el número más pequeño.
+let numeros = [-1, -3, -4400, -12, -600]; //aqui se declara los numeros del arreglo
+console.log("El número menor en el arreglo " + numeros + " es: ");// este es el msj que mandara la consola del resultado
+console.log(numMenor(numeros));//aqui se inserta la variable a la funcion 
 
-function numMayor(x) {//aqui se realiza la funcion que se va a utlizar despues a nuestro let
-  let max = x[0];// se declara la variable para la funcion
-  for (let i=0; i < x.length; i++) {
-    if (x[i]> max) {
-      max = x[i];
+function numMenor(x) {//aqui se realiza la funcion que se va a utlizar despues a nuestro let
+  let min = x[0];// se declara la variable para la funcion
+  for (let i=-2; i < x.length; i++) {
+    if (x[i]< min) {
+      min = x[i];
     }//if
   } //for if
-  return max;
-}//numMayor
+  return min;
+}//numMenor
 
